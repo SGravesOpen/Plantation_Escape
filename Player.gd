a3e8fb03c1c3da717841537e12d0da54641272fc
Sam <Mashard30@gmail.com>
2020-02-03 00:43:42 -0600

    started hea;th
    health start

--- Player.gd
+++ Player.gd
@@ -3,6 +3,10 @@ extends KinematicBody2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+signal health_updated(health)
+signal killed()
+
+
 const UP = Vector2(0,-1)
 const GRAVITY = 20
 const acceleration = 50
@@ -12,7 +16,9 @@ var motion=Vector2()
 
 var score = 0
 
+export (float) var max_health = 100
 
+onready var health = max_health setget _set_health
 
 func _process(delta):
 	var LabelNode = get_parent().get_node("Score Counter/UI/Control/RichTextLabel")
@@ -68,3 +74,20 @@ func _physics_process(delta):
 func _on_Coin_0_body_entered(body):
 	score + 1
 	pass # Replace with function body.
+	
+func damage(amount):
+	_set_health(health - amount)
+
+
+
+func kill():
+	pass	
+	
+func _set_health(value):
+	var prev_health = health
+	health = clamp(value, 0, max_health)
+	if health != prev_health:
+		emit_signal("health_updated",health)
+		if health == 0:
+			kill()
+			emit_signal("killed")
